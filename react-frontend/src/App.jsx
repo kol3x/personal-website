f1470a7901b39c8ad0bc7154f933e6ed310cbb29
kol3x <kolaegipet@gmail.com>
2024-03-05 13:52:48 +0400

    made global fadein faster

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -10,7 +10,7 @@ import { motion } from "framer-motion";
 function App() {
   const fadeInVariants = {
     hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 7 } },
+    visible: { opacity: 1, transition: { duration: 3 } },
   };
   return (
     <motion.div initial="hidden" animate="visible" variants={fadeInVariants}>
