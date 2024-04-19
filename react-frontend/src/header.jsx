5b3930f8c35b5f40466072c7e9a43be3eaacb22a
kol3x <kolaegipet@gmail.com>
2024-04-19 23:55:12 +0400

    added new hero screen, removed bg from projects section

--- react-frontend/src/header.jsx
+++ react-frontend/src/header.jsx
@@ -1,7 +1,25 @@
+import HeroBackground from "./imgs/heroBackground.jpg";
+import NikoPhotoAnimated from "./imgs/NikoPhotoAnimated.gif";
+
+const style = {
+  backgroundImage: `url(${HeroBackground})`,
+};
+
 function Header() {
-  return <header>
-    
-  </header>;
+  return (
+    <header style={style}>
+      <h1 className="name">
+        NIKOLAI <br></br> <span>SHCHERBININ</span>
+        <h4 className="jobTitle">SOFTWARE DEVELOPER</h4>
+      </h1>
+      <img
+        className="nikoAnimatedPhoto"
+        src={NikoPhotoAnimated}
+        alt="Nikolai Animated"
+      />
+      
+    </header>
+  );
 }
 
 export default Header;
