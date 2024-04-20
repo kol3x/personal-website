83baeef6b6d2823c96c9334b57c38ed9f619b78e
kol3x <kolaegipet@gmail.com>
2024-04-20 14:48:06 +0400

    changed desc of personal website projcet

--- react-frontend/src/portfolio.jsx
+++ react-frontend/src/portfolio.jsx
@@ -30,7 +30,7 @@ const projects = [
   },
   {
     title: "Personal Website",
-    desc: "Simple nice personal website, made with React, Framer Motion, MaterialUI. It went throw many variations, but came down to this.",
+    desc: "Well, you are here right now. Simple nice personal website, made with React, Framer Motion, MaterialUI. It went throw many variations, but came down to this.",
     stack: "React, Framer Motion, MaterialUI",
     img: PersonalWebsiteImg,
     appURL: "./",
